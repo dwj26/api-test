b9f54371ceb753aae24df4421cacdf25e833acfb
dwj26 <danielwilliamjones1994@gmail.com>
2018-04-01 19:31:57 +0100

    extract game size

--- memory/public/javascripts/scripts.js
+++ memory/public/javascripts/scripts.js
@@ -32,7 +32,11 @@ function newGame() {
     gameBoardSize = 0;
     cardsFlipped = 0;
 
-    // Add code from Part 2.2 here
+    // extract game size selection from user
+    var size = $("#selectGameSize").val();
+
+    // parse game size value
+    size = parseInt(size, 10);
 
 
     // check if this is a valid selection
